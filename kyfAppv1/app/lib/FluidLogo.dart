fa95d6d34d5be59a953a83ad8a48b05577de3227
Bobcati <108192537+Bobcati@users.noreply.github.com>
2022-07-28 16:22:37 +0100

    Know your farmer basic homepage prototype

---
+++ kyfAppv1/app/lib/FluidLogo.dart
@@ -0,0 +1,12 @@
+class FluidLogo {
+  String imageUrl;
+
+  FluidLogo(
+    this.imageUrl,
+  );
+  static List<FluidLogo> samples = [
+    FluidLogo(
+      'assets/fluidlogo.png',
+    ),
+  ];
+}
